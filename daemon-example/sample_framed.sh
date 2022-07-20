8f69ad48806203f2d914a20f6a7eb38294a0ace0
Natasha Kubiak <natashaskubiak@gmail.com>
2022-07-19 21:54:00 -0600

    sample-framed

---
+++ daemon-example/sample_framed.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+DAEMON_NAME="SAMPLE-DAEMON"
+
+start() {
+    printf "Starting $DAEMON_NAME: "
+    echo "OK"
+}
+
+stop() {
+    printf "Stopping $DAEMON_NAME: "
+    echo "OK"
+}
+
+restart() {
+    stop
+    start
+}
+
+
+case "$1" in
+
+    start)
+    start
+    ;;
+    stop)
+    stop
+    ;;
+    retsrt|reload)
+    restart
+    ;;
+    *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+
+exit $?
