bc58a816efd0eae962fd8e9d9dc3c4bebd1e0492
Natasha Kubiak <natashaskubiak@gmail.com>
2022-07-22 17:20:45 -0600

    Daemon stuff/syntax

--- daemon-example/daemon_script.sh
+++ daemon-example/daemon_script.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-DAEMON_NAME="SAMPLED"
+DAEMON_NAME="sampled"
 
 start() {
     printf "Starting $DAEMON_NAME: "
-    # /usr/sbin/$DAEMON_NAME
-    # touch /var/lock/$DAEMON_NAME
+    /usr/sbin/$DAEMON_NAME
+    touch /var/lock/$DAEMON_NAME
     echo "OK"
 }
 
 stop() {
     printf "Stopping $DAEMON_NAME: "
-    # killall $DAEMON_NAME
-    # rm -f /var/lock/$DAEMON_NAME
+    killall $DAEMON_NAME
+    rm -f /var/lock/$DAEMON_NAME
     echo "OK"
 }
 
