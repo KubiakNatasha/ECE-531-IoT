161c3177f7e88f51ec7b9836ca4fe312444a7010
Natasha Kubiak <natashaskubiak@gmail.com>
2022-07-23 12:02:48 -0600

    project file

---
+++ THERMO-PROJECT/thermostatd.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+
+
+#define DAEMON_NAME     "thermostatd"
+
+#define OK              0
+#define ERR_SETSID      1
+#define ERROR           2
+#define ERR_FORK        3
+#define ERR_CHDIR       4
+#define true            0
+/*Case Values*/
+#define SIGTERM         2
+#define SIGHUP          3
+/*Error Format*/
+char *ERROR_FORMAT = "Format Error";
+
+
+
+/****************Define functions**********/
+void _signal_handler(const int signal);
+void _do_work(void);
+/******************************************/
+
+
+int main(void) {
+
+
+    openlog(DAEMON_NAME, LOG_PID | LOG_NDELAY | LOG_NOWAIT, LOG_DAEMON);
+    syslog(LOG_INFO, "starting sampled");
+    
+
+    /* fork off the parent process*/
+    pid_t pid = fork();
+    
+    //check if child
+    if (pid < 0){
+        syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+        return ERR_FORK;
+    }
+    // check if parent process, continue
+    if (pid > 0){
+        return OK;
+    }
+
+
+
+    if(setsid() < -1) {
+        syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+        return ERR_SETSID;
+    }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+
+    umask(S_IRUSR | S_IWUSR | S_IRGRP |S_IROTH);
+
+    //Set the working directory.
+    //Set to Root to make navigation easy
+
+    if(chdir("/") < 0) {
+        syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+        return ERR_CHDIR;
+    }
+
+    //signal handling
+
+    signal(SIGTERM, _signal_handler);
+    signal(SIGHUP, _signal_handler);
+    
+    _do_work();
+
+
+    return ERROR;
+
+}
+
+    //work will be done by daemon
+    //counts and sleeps
+    //declared as non-static
+void _do_work(void){
+  
+
+    for (int i = 0; 100; i++){
+
+        syslog(LOG_INFO, "iteration:%d", i);
+        sleep(1);
+    }
+}
+
+
+
+    //signal handler which will handle any signals passed
+    //declared as non static
+void _signal_handler(const int signal) {
+
+    switch(signal) {
+
+        case SIGHUP:
+            break;
+
+        case SIGTERM:
+            syslog(LOG_INFO, "recieved SIGTERM, exiting.");
+            closelog();
+            exit(OK);
+            break;
+
+        default:
+            syslog(LOG_INFO, "recieved unhandled signal.");
+            closelog();
+            exit(OK);
+
+    }
+}
