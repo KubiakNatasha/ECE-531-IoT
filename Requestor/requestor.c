8cfe7f0be346b38f5da25294641b0519e60b6f1d
Natasha Kubiak <natashaskubiak@gmail.com>
2022-07-25 16:25:55 -0600

    requestor

--- Requestor/requestor.c
+++ Requestor/requestor.c
@@ -2,13 +2,16 @@
 #include <curl/curl.h>
 #include <string.h>
 #include <argp.h>
+/* Information on argp.h used from tutorial : http://nongnu.askapache.com/argpbook/step-by-step-into-argp.pdf */
+
 
 #define OK          0
 #define INIT_ERR    1
 #define REQ_ERR     2
 #define default		0
+/* Host OS, Virtual Box*/
+#define URL         "http://10.0.2.15:8000" 
 
-#define URL         "http://localhost:8000"
 
 /*********PROTOTYPE**************/
 void HELP();
@@ -28,6 +31,7 @@ int main(int argc, char **argv) {
     if(argc <= 1) {
 			printf("Empty Argument.\n");
 			printd("Exiting...\n");
+			return INIT_ERR;
 			
 		}
 	/* If Arguments is greater than 6*/
@@ -48,35 +52,8 @@ int main(int argc, char **argv) {
 		{0}
 	};
 	
-	switch (argc)
-		{
-			case 'g':
-			case 'get':
-				GET();
-				break;
-			case 'p':
-			case 'put':
-				PUT();
-				break;
-			case 'o':
-			case 'post':
-				POST();
-				break;
-			case 'd':
-			case 'delete':
-				DELETE();
-				break;
-			case 'h':
-			case 'help':
-				Help();
-				break;
-
-			default:
-				printf("Invalid or Unkown Argument.\n"); 
-				printd("For HELP, please type in: ./hw -h , or ./hw --help\n"); 
-				return INIT_ERR;
-		}
-
+	struct argp argp = { options, parse_opt };
+	return argp_parse (&argp, argc, argv, 0, 0, 0);
 	return OK;
 }
 
@@ -199,8 +176,86 @@ void DELETE(char *url, CURL *curl, CURLcode res, char *postdata) {
 /*************************************/
 static int parse_opt (int key, char *arg, struct argp_state *state)
 {
- switch (key)
- {
- case 'd': printf (".\n"); break;
- }
- re
+	int i;
+	
+
+  switch (key)
+  {
+	/* URL */
+    case 'u':
+	case 'url':
+	case 'URL':
+    {
+      printf("Set URL\n");
+      break;
+    }
+
+	/*POST*/
+    case 'o':
+	case 'post':
+      printf("Post\n");
+	  break;
+
+	/*GET*/
+    case 'g':
+	case 'get': 
+    {
+      i = GET(); 
+      printf ("GET = %d\n", i);
+      break;
+    }
+
+	/*PUT*/
+    case 'p':
+	case 'put': 
+    {
+      printf("PUT\n");
+    }
+
+	/*DELETE*/
+    case 'd':
+	case 'delete': .
+    {
+      printf("Delete\n");
+      break;
+    }
+
+	case 'h':
+	case 'help':
+		printf("REQUESTOR HELP:\n");
+		printf("-------------------------\n\n");
+		printf("Options:\n");
+		printf("HTTP REQUESTS:\n");
+		printf("  -g  --get    GET      \n");
+		printf("  -o  --post   POST     \n");
+		printf("  -p  --put    PUT      \n");
+		printf("  -d  --delete DELETE   \n");
+		printf(" HTTP REQUESTS should be followed with a valid URL\n");
+		printf(" -u   --url    URL \n");
+		printf("-------------------------\n\n");
+		printf("COMMAND LINE INPUT FORMAT\n");
+		printf("hw [REQUEST] --url [URL]\n");
+		printf("EXAMPLES\n");
+		printf("hw --post --url http://localhost:8080 \n");
+		printf("hw --get --url http://www.cnn.com\n");
+		printf("hw --put --url http://localhost:8080\n");
+		printf("hw --delete --url http://localhost:8080 78392\n");
+		printf("-------------------------\n\n");
+		printf("Exit definitions:\n");
+		printf("0 OK\n");
+		printf("1 INIT_ERR\n");
+		printf("2 REQ_ERR\n");
+		printf("-------------------------\n\n");
+		printf("To display help again:");
+		printf("hw -h --help");
+		break;
+
+
+	default:
+		printf("Invalid or Unkown Argument.\n"); 
+		printd("For HELP enter: ./hw -h , or ./hw --help\n"); 
+		return INIT_ERR;
+  }
+  return 0;
+}
+
