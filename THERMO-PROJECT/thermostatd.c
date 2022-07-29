8de6b2023c401bd9fe2a8f07e30ce4f1b9b93e55
Natasha Kubiak <natashaskubiak@gmail.com>
2022-07-29 13:51:27 -0600

    project

--- THERMO-PROJECT/thermostatd.c
+++ THERMO-PROJECT/thermostatd.c
@@ -209,7 +209,7 @@ void HeaterStatus()
 			FILE *filep;
 			filep = fopen("/tmp/status", "wb");
 			char *status = "OFF";
-			fprintf(filep, "%s at time:%02d:%02d", status, hours,minutes);
+			fprintf(filep, "%s at time:%02d:%02d", status, hour,min);
 			fclose(filep);
 			syslog(LOG_INFO, "OFF\n"); 
 
@@ -219,7 +219,7 @@ void HeaterStatus()
 			FILE *filep;
 			filep = fopen("/tmp/status", "wb");
 			char *status = "ON";
-			fprintf(filep, "%s at time:%02d:%02d", status, hours,minutes);
+			fprintf(filep, "%s at time:%02d:%02d", status, hour,min);
 			fclose(filep);
 			syslog(LOG_INFO, "ON\n"); 
                     /* need a timestamp */
