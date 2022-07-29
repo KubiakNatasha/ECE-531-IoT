43a1a1236f6165914201c573afe80f4e347a599f
Natasha Kubiak <natashaskubiak@gmail.com>
2022-07-29 16:10:02 -0600

    project

---
+++ THERMO-PROJECT/thermo-arm/project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+printf "Thermostat Simulation for ECE 531 - Intro to Iot\n"
+printf "Natasha Kubiak\n"
+printf "SUMMER 2022\n"
+sleep 5
+
+
+cat credentials.txt
+printf "Log into VM using Credentials...\n"
+sleep 5
+printf "Starting Image VM...\n"
+sleep 5
+cd buildroot/
+./qemu-versatile-shell.sh
